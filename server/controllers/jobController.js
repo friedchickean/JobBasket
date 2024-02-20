9e22ca51ffc31b45d10e2ab8a135b0297e37d46c
friedchickean <kdarg29@gmail.com>
2024-02-19 21:46:54 -0700

    Initial backend structure complete

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,8 +1,25 @@
 const Job = require('../models/JobModel');
+const mongoose = require('mongoose');
 
 // get all job
+const getJobs = async (req, res) => {
+    const jobs = await Job.find({}).sort({createdAt: -1});
+    res.status(200).json(jobs);
+}
 
 // get a single job
+const getJob = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'ID not valid'});
+    }
+    const job = await Job.findById(id);
+    if (!job) {
+        res.status(400).json({error: "no such job"});
+    }
+    res.status(200).json(job);
+}
+
 
 // create new job
 const createJob = async (req, res) => {
@@ -18,8 +35,44 @@ const createJob = async (req, res) => {
 }
 
 // delete a job
+const deleteJob = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'ID not valid'});
+    }
+
+    const job = await Job.findOneAndDelete({_id: id});
+
+    if (!job) {
+        res.status(400).json({error: "no such job"});
+    }
+    res.status(200).json(job);
+}
 
 // update a job
+const updateJob = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'ID not valid'});
+    }
+
+    // spread the body to the fields
+    const job = await Job.findOneAndUpdate({_id: id}, {
+        ...req.body
+    });
+
+    if (!job) {
+        res.status(400).json({error: "no such job"});
+    }
+    res.status(200).json(job);
+
+}
+
 module.exports = {
-    createJob
+    getJob,
+    getJobs,
+    createJob,
+    deleteJob,
+    updateJob
 }
--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -1,31 +1,22 @@
 const express = require('express');
-const { createJob } = require('../controllers/jobController');
+const { getJob, getJobs, createJob, deleteJob, updateJob } = require('../controllers/jobController');
 
 const router = express.Router();
 
 // GET all job
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all job'});
-});
-
+router.get('/', getJobs);
 
 // GET single job
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'GET a job'});
-});
+router.get('/:id', getJob);
 
 // POST new job
 router.post('/', createJob);
 
 // DELETE a job
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETE a job'});
-});
+router.delete('/:id', deleteJob);
 
 // UPDATE a job
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'UPDATE a job'});
-});
+router.patch('/:id', updateJob);
 
 
 module.exports = router;
